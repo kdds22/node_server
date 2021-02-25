38571f161d72147137e38a2185cf7d03d4029cb8
kdds22 <klaus.dd.sa@gmail.com>
2021-02-25 18:59:18 -0300

    Implementing Controllers, Routes, Models - typeorm - Serialization - Database

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,11 @@
 import express from 'express';
+import 'reflect-metadata';
+import './database';
+import { router } from './Routes';
 
 const app = express();
 
 app.use(express.json())
-
-
-app.get("/", (req, res) => {
-    return res.json({ message: "Hello GET data Node_NLW#4" });
-});
-
-app.post("/", (req, res) => {
-    return res.json({ message: "Hello POST data in Node_NLW#4", data: req.body });
-});
-
+app.use(router);
 
 app.listen(3333, () => console.log('Server is running...'));
